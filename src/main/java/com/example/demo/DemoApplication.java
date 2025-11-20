b18d55daf6d9381eebd5608004aeb0a61c12ea45
praveen <praveenk@gmail.com>
2025-11-20 14:54:27 +0530

    added new apiend point

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -12,6 +12,11 @@ public class DemoApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
+	
+	@GetMapping("/suspend")
+	public String suspend() {
+		return "Application is suspended";
+	}
 
 	
 	@GetMapping("/start")
